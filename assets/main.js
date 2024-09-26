d641abea064073f10a0b3100ad779513cd5a111a
yalnguembu <yalnguembu@gmail.com>
2024-09-26 07:38:07 +0100

    end

--- assets/main.js
+++ assets/main.js
@@ -9,27 +9,39 @@ const swiper = new Swiper(".reviews-1", {
   },
 });
 
-const titles = document.querySelectorAll(".head");
-const contents = document.querySelectorAll(".body");
-const icons = document.querySelectorAll(".circle-icon");
+addEventListener("DOMContentLoaded", () => {
+  const titles = document.querySelectorAll(".head");
+  const contents = document.querySelectorAll(".faq .body");
+  const allMinus = document.querySelectorAll(".minus");
+  const allPlus = document.querySelectorAll(".plus");
 
-titles.forEach((titre, index) => {
-  titre.addEventListener("click", () => {
-    if (contents[index].classList.contains("visible")) {
-      contents[index].classList.remove("visible");
-      const minus = titre.querySelector(".minus");
-      const plus = titre.querySelector(".plus");
-      if (minus) minus.style.display = "none";
-      if (plus) plus.style.display = "block";
-    } else {
-      contents.forEach((contenu) => {
-        contenu.classList.remove("visible");
-      });
-      contents[index].classList.add("visible");
-      const minus = titre.querySelector(".minus");
-      const plus = titre.querySelector(".plus");
-      if (minus) minus.style.display = "block";
-      if (plus) plus.style.display = "none";
-    }
+  titles.forEach((titre, index) => {
+    titre.addEventListener("click", () => {
+
+      if (contents[index].classList.contains("visible")) {
+
+        contents[index].classList.remove("visible");
+        const minus = titre.querySelector(".minus");
+        const plus = titre.querySelector(".plus");
+        if (minus) minus.style.display = "none";
+        if (plus) plus.style.display = "block";
+
+      } else {
+
+        contents.forEach((contenu) => {
+          contenu.classList.remove("visible");
+        });
+        allMinus.forEach((item, minusIndex) => {
+          item.style.display = minusIndex === index ? "block" : "none";
+        });
+        allPlus.forEach((item) => {
+          item.style.display = "block";
+        });
+
+        contents[index].classList.add("visible");
+        const plus = titre.querySelector(".plus");
+        if (plus) plus.style.display = "none";
+      }
+    });
   });
 });
