208d20990cfe340dc879b621fa7fd82b62cdbfa8
yalnguembu <yalnguembu@gmail.com>
2024-09-26 03:47:52 +0100

    slider in progress

---
+++ assets/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".reviews-1", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".first-button-next",
+    prevEl: ".first-button-prev",
+  },
+});
